f4011fe7d81cd6c89619161997a163ee361c7de4
Leander Christmann <l.christmann@pharma4u.de>
2025-05-19 13:58:36 +0200

    implement querying for invoices, extract logic, make pagination links preserve query strings (58:35 in course)

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Filters\V1\CustomerFilter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
-use App\Services\V1\CustomerQuery;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -18,15 +18,14 @@ class CustomerController extends Controller
      */
     public function index(Request $request): CustomerCollection
     {
-//        dump($request);
-
-        $filter = new CustomerQuery();
+        $filter = new CustomerFilter();
         $queryItems = $filter->transform($request); // [['column', 'operator', 'value'], ...]
 
         if (count($queryItems) === 0) {
             return new CustomerCollection(Customer::paginate());
         } else {
-            return new CustomerCollection(Customer::where($queryItems)->paginate());
+            $customers = Customer::where($queryItems)->paginate();
+            return new CustomerCollection($customers->appends($request->query()));
         }
     }
 
